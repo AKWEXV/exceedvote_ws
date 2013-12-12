a9cc0c62a6a733cbc62d2f566ccadb75d9174d94
sonnyhonny <sonnyhonny@gmail.com>
2013-12-12 11:10:50 +0700

    Update myvote.

---
+++ exceedvote/app/controllers/Rank.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.*;
+import play.data.*;
+import play.mvc.*;
+
+import models.*;
+import views.html.*;
+import views.xml.*;
+
+public class Rank extends Controller {
+
+}
--- exceedvote/app/models/Criterion.java
+++ exceedvote/app/models/Criterion.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.*;
+
 import javax.persistence.*;
 
 import play.db.ebean.*;
