e35f68625fe1d855396abeb3946e436c9075da2f
kimapiwat <sri.apiwat@gmail.com>
2013-12-29 11:11:36 +0700

    sql

--- exceedvote/conf/evolutions/default/1.sql
+++ exceedvote/conf/evolutions/default/1.sql
@@ -4,42 +4,35 @@
 # --- !Ups
 
 create table ballot (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   contestant_id             bigint,
   score                     integer,
   constraint pk_ballot primary key (id))
 ;
 
 create table contestant (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   description               varchar(255),
   constraint pk_contestant primary key (id))
 ;
 
 create table criterion (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   type                      integer,
   constraint pk_criterion primary key (id))
 ;
 
 create table role (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   criterion_vote            integer,
   constraint pk_role primary key (id))
 ;
 
-create table timer (
-  id                        bigint not null,
-  start                     timestamp,
-  finish                    timestamp,
-  constraint pk_timer primary key (id))
-;
-
 create table user (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   role_id                   bigint,
   contestant_id             bigint,
   username                  varchar(255),
@@ -49,7 +42,7 @@ create table user (
 ;
 
 create table vote (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   user_id                   bigint,
   criterion_id              bigint,
   constraint pk_vote primary key (id))
@@ -61,20 +54,6 @@ create table vote_ballot (
   ballot_id                      bigint not null,
   constraint pk_vote_ballot primary key (vote_id, ballot_id))
 ;
-create sequence ballot_seq;
-
-create sequence contestant_seq;
-
-create sequence criterion_seq;
-
-create sequence role_seq;
-
-create sequence timer_seq;
-
-create sequence user_seq;
-
-create sequence vote_seq;
-
 alter table ballot add constraint fk_ballot_contestant_1 foreign key (contestant_id) references contestant (id) on delete restrict on update restrict;
 create index ix_ballot_contestant_1 on ballot (contestant_id);
 alter table user add constraint fk_user_role_2 foreign key (role_id) references role (id) on delete restrict on update restrict;
@@ -94,38 +73,21 @@ alter table vote_ballot add constraint fk_vote_ballot_ballot_02 foreign key (bal
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists ballot;
+drop table ballot;
 
-drop table if exists contestant;
+drop table contestant;
 
-drop table if exists criterion;
+drop table criterion;
 
-drop table if exists role;
-
-drop table if exists timer;
-
-drop table if exists user;
-
-drop table if exists vote;
-
-drop table if exists vote_ballot;
-
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists ballot_seq;
-
-drop sequence if exists contestant_seq;
+drop table role;
 
 drop table user;
-drop sequence if exists criterion_seq;
 
-drop sequence if exists role_seq;
+drop table vote;
 
-drop sequence if exists timer_seq;
+drop table vote_ballot;
 
-drop sequence if exists user_seq;
-
-drop sequence if exists vote_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
